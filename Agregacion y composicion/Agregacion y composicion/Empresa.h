3c00dc01229ac0da019b183dfc4c86c50c45a838
a22310439 <111840306+a22310439@users.noreply.github.com>
2023-03-28 17:11:31 -0600

    Agregacion y composicion correcto
    Se agrega el codigo correcto de Agregacion y composicion

---
+++ Agregacion y composicion/Agregacion y composicion/Empresa.h
@@ -0,0 +1,22 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include"Cliente.h"
+#include"Empleado.h"
+
+using namespace std;
+
+class Empresa {
+private:
+    vector<Empleado*> listaEmpleados;
+    Cliente* cliente;
+
+public:
+    Empresa();
+    Empresa(Cliente* cli);
+    ~Empresa();
+    void agregarEmpleado(Empleado* emp);
+    double getSumaSueldos();
+    Cliente* getCliente();
+    void mostrarDatos();
+};
